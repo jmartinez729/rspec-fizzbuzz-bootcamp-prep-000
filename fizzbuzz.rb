c28bb8047780e283c15e92fa111e87afdf4294db
John Martinez <jmartinez729@gmail.com>
2018-08-20 20:31:03 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,6 +2,15 @@
 # See README.md for instructions on how to do this
 
 
-def fizzbuzz
+def fizzbuzz(int)
+  if int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+  elsif int % 5 == 0 && int % 3 == 0 
+    return "Fizzbuzz"
+  else 
+    return nil 
+  end 
   
 end 
